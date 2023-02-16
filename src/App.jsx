34e7c6ae432caa2e14dec5c5d21918bcb1bdeea9
jaikumarPristynCare <jai.kumar@pristyncare.com>
2023-02-16 17:53:46 +0530

    second Day

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { createRoot } from 'react-dom';
+import Pet from './Pet';
+const App = () => {
+    <div>
+        <h1>Adopt Me!</h1>
+        <Pet name="Luna" animal="dog" breed="Havanese" />
+        <Pet name="Pepper" animal="bird" breed="Cockatiel" />
+        <Pet name="Doink" animal="cat" breed="Mix" />
+    </div>
+}
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
